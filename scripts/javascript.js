d93845c2066c85be113f367a3cab54830ad80c76
janjeyrald <u195595992@byui.edu>
2021-01-09 17:04:50 +0800

    js

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -26,6 +26,6 @@ let months = [
 let d = new.Date();
 let dayName = daynames[d.getDay()];
 let monthName = months[d.getMonth()];
-let fulldate = dayName + ", " + monthName + " " + d.getDate() + ", "+ d.getFullYear();
+let fullDate = dayName + ", " + monthName + " " + d.getDate() + ", "+ d.getFullYear();
 
-document.getElementById("currentdate").textContent = fulldate;
+document.getElementById("currentdate").textContent = fullDate;
