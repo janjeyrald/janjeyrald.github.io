aa5aedd87db7804edba58383723bdada15a448e2
janjeyrald <u195595992@byui.edu>
2021-01-09 20:19:51 +0800

    js

--- scripts/datetoday.js
+++ scripts/datetoday.js
@@ -1,4 +1,27 @@
+let days =[
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
 
+let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
 
 let d = new Date();
 let dayName = days[d.getDay()];
