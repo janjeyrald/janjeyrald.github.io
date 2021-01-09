076a872362e86aa8c5821c07cec1a71ab5f2c099
janjeyrald <u195595992@byui.edu>
2021-01-09 19:51:20 +0800

    js

--- scripts/datetoday.js
+++ scripts/datetoday.js
@@ -26,8 +26,9 @@ let months = [
 let d = new Date();
 let dayName = days[d.getDay()];
 let monthName = months[d.getMonth()];
+let year= d.getFullYear();
 let fulldate = dayName + ", " + monthName + " " + d.getDate() + ", "+ d.getFullYear();
 
 document.getElementById("currentdate").textContent = fulldate;
 
-document.getElementById("currentyear").textContent = d.getFullYear();
+document.getElementById("currentyear").textContent = year();
