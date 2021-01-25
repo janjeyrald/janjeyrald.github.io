4bf0326cd4e5ba9337e09390aafcb0f568dd5495
janjeyrald <u195595992@byui.edu>
2021-01-25 15:44:28 +0800

    done

---
+++ lesson3/scripts/datetoday.js
@@ -0,0 +1,37 @@
+let days =[
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+
+let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+
+let d = new Date();
+let dayName = days[d.getDay()];
+let monthName = months[d.getMonth()];
+let year= d.getFullYear();
+let fulldate = dayName + ", " + monthName + " " + d.getDate() + ", "+ d.getFullYear();
+
+document.getElementById("currentdate").textContent = fulldate;
+
+document.getElementById("currentyear").textContent = year;
+
+let oLastModif = (document.lastModified);
+document.getElementById("lastupdate").innerHTML = oLastModif;
