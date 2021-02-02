974c7a3530b38c7b4f5b420e2f98ede65ec8efa6
janjeyrald <u195595992@byui.edu>
2021-02-02 13:33:42 +0800

    lesson 5

---
+++ lesson5/js/lastmodified.js
@@ -0,0 +1,37 @@
+let oLastModif = (document.lastModified);
+document.getElementById("lastupdate").innerHTML = oLastModif;
+
+let days =[
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+
+let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+
+let d = new Date();
+let dayName = days[d.getDay()];
+let monthName = months[d.getMonth()];
+let year= d.getFullYear();
+let fulldate = dayName + ", " + monthName + " " + d.getDate() + ", "+ d.getFullYear();
+
+document.getElementById("currentyear").textContent = year;
+
+document.getElementById("currentdate").textContent = fulldate;
