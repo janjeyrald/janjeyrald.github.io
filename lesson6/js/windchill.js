c78cadef7b510258fe262ea57cff718d07284c2f
janjeyrald <u195595992@byui.edu>
2021-02-16 15:34:35 +0800

    js

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -2,11 +2,7 @@ let t = parseFloat(document.getElementById("currentTemp").innerHTML);
 let w = parseFloat(document.getElementById("windSpeed").innerHTML);
 
 if (t <= 50 && w >= 3) {
-  let result =
-    35.74 +
-    0.6215 * t -
-    35.75 * Math.pow(w, 0.16) +
-    0.4275 * t * Math.pow(w, 0.16);
+  let result =35.74 + 0.6215 * t - 35.75 * Math.pow(w, 0.16) + 0.4275 * t * Math.pow(w, 0.16);
 
   result = document.getElementById("windChillInput").innerHTML =
     Math.round(result) + "&deg; " + "F";
