a09cb0748075b2481da4c3c4d70410d1b19e2aae
janjeyrald <u195595992@byui.edu>
2021-03-09 22:15:42 +0800

    push js

---
+++ lesson-9/js/cards.js
@@ -0,0 +1,49 @@
+const requestURL = "https://byui-cit230.github.io/weather/data/towndata.json";
+
+fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+
+.then(function (jsonObject) {
+
+const towns = jsonObject['towns'];
+    
+
+for (let i=0; i < towns.length; i++) {
+  
+    let card = document.createElement('section');
+    let h2 = document.createElement('h2');
+    let motto = document.createElement('h3');
+    let year = document.createElement('p');
+    let pop = document.createElement('p');
+    let rain = document.createElement('p');
+    let image = document.createElement('img');
+    let textDiv = document.createElement('div')
+    
+    if ((towns[i].name == 'Preston')|| (towns[i].name == 'Fish Haven') || (towns[i].name == 'Soda Springs')) {
+    
+    h2.textContent = towns[i].name;
+    motto.textContent = towns[i].motto;    
+    year.textContent = "Year Founded:" + " " + towns[i].yearFounded;
+    pop.textContent = "Population:" + " " + towns[i].currentPopulation;
+    rain.textContent = "Annual Rainfall:" + " " +  towns[i].averageRainfall;
+    image.setAttribute('src', `images/${towns[i].photo}`);
+    image.setAttribute('class', 'townImages');
+    image.setAttribute('alt', towns[i].name+ "("+towns[i].motto + ")");
+    card.setAttribute('class', 'homepageSection' );
+    textDiv.setAttribute('class', 'homepageInfo');
+
+        card.appendChild(textDiv);
+        textDiv.appendChild(h2);
+        textDiv.appendChild(motto);
+        textDiv.appendChild(year);
+        textDiv.appendChild(pop);
+        textDiv.appendChild(rain);
+        card.appendChild(image);}
+
+    
+        
+     document.querySelector('div.idahoTowns').appendChild(card);
+}
+});
