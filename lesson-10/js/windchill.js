1b69a39d15d4ad4a995099ff80703f756025b4aa
janjeyrald <u195595992@byui.edu>
2021-03-15 13:55:27 +0800

    preston

--- lesson-10/js/windchill.js
+++ lesson-10/js/windchill.js
@@ -5,21 +5,19 @@ fetch(apiURL)
   .then((jsObject) => {
     console.log(jsObject);
     //Get output the info in current-temp ID from APIURL .main.temp
-    let t = document.getElementById("currentTemp").textContent = jsObject.main.temp;
+    let temp = document.getElementById("currentTemp").textContent = jsObject.main.temp;
     document.getElementById("humidityResult").textContent = jsObject.main.humidity + "%";
-    let w = document.getElementById("windSpeed").textContent = jsObject.wind.speed;
+    let ws = document.getElementById("windSpeed").textContent = jsObject.wind.speed;
     document.getElementById("condition").textContent = jsObject.weather[0].description;
     const imagesrc = "https://openweathermap.org/img/w/" + jsObject.weather[0].icon + ".png"; // note the concatenation
     const desc = jsObject.weather[0].description; // note how we reference the weather array
-    document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
-    document.getElementById('icon').setAttribute('alt', desc);
 
-    if (t <= 50 && w >= 3) {
+    if (temp <= 50 && ws >= 3) {
       let result =
         35.74 +
-        0.6215 * t -
-        35.75 * Math.pow(w, 0.16) +
-        0.4275 * t * Math.pow(w, 0.16);
+        0.6215 * temp -
+        35.75 * Math.pow(ws, 0.16) +
+        0.4275 * temp * Math.pow(ws, 0.16);
 
       result = document.getElementById("windChillInput").innerHTML =
         Math.round(result) + "&deg; " + "F";
