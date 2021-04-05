9b17f76d8007f1b6c7282db96a245692ffcefe2b
janjeyrald <u195595992@byui.edu>
2021-04-05 15:12:51 +0800

    business json done js

--- FinalProject/js/grid.js
+++ FinalProject/js/grid.js
@@ -15,60 +15,33 @@ fetch(requestURL)
         // Declare value for each variable needed
      
             let card = document.createElement('section');
-            let card2 = document.createElement('section');
             let h2 = document.createElement('h2');
             let cInfo = document.createElement('p');
             let page = document.createElement('p');
             let bP = document.createElement('img');
             let visit = document.createElement('a');
-            let h2Two = document.createElement('h2');
-            let cInfoTwo = document.createElement('p');
-            let pageTwo = document.createElement('p');
-            let bPTwo = document.createElement('img');
-            let visitTwo = document.createElement('a');
         //Set output string, style and attribute of each cards
-            card.setAttribute('class', "view_item");
-            
+            card.setAttribute('class', "view_item")
             h2.textContent = business[i].name;
             h2.setAttribute('class', "title");
             cInfo.textContent = "Contact Info:" +  " " + business[i].contactInformation;  
             page.textContent = "Facebook Page:" + " " + business[i].fbPage;
             bP.setAttribute('src', `images/${business[i].photo}`);
             bP.setAttribute("alt", business[i].name + " " + business[i].lastname + " " + "-" + " " + business[i].order);
+            bP.style.boxShadow = "2px 2px 2px #F3F3F4";
+            bP.style.width="200px";
             visit.textContent = "Visit";
             visit.setAttribute('target', '_blank')
             visit.setAttribute('class', 'btn')
             visit.setAttribute('href', business[i].fbPage)
-
-           
-            card2.setAttribute('class', "view_item2");
-            h2Two.textContent = business[i].name;
-            h2Two.setAttribute('class', "title");
-            cInfoTwo.textContent = "Contact Info:" +  " " + business[i].contactInformation;  
-            pageTwo.textContent = "Facebook Page:" + " " + business[i].fbPage;
-            bPTwo.setAttribute('src', `images/${business[i].photo}`);
-            bPTwo.setAttribute("alt", business[i].name + " " + business[i].lastname + " " + "-" + " " + business[i].order);
-            visitTwo.textContent = "Visit";
-            visitTwo.setAttribute('target', '_blank')
-            visitTwo.setAttribute('class', 'btn')
-            visitTwo.setAttribute('href', business[i].fbPage)
         //Append text information and fill everything from the loop
-           
             card.append(h2);
             card.append(bP);
             card.appendChild(cInfo);
             card.appendChild(page);
             card.appendChild(visit);
-            document.querySelector('div.listDirectory').appendChild(card);
-        
-            card2.append(h2Two);
-            card2.append(bPTwo);
-            card2.appendChild(cInfoTwo);
-            card2.appendChild(pageTwo);
-            card2.appendChild(visitTwo);
-            document.querySelector('div.gridDirectory').appendChild(card2);
-        
+            
+            document.querySelector('div.gridDirectory').appendChild(card);
         }
-        
     });
 
