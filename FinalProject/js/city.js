bcd4cb0f6a173ccc2cb6d137cc567227b53e59da
janjeyrald <u195595992@byui.edu>
2021-04-06 13:37:55 +0800

    images

--- FinalProject/js/city.js
+++ FinalProject/js/city.js
@@ -12,26 +12,31 @@ const city = jsonObject['city'];
 
 for (let i=0; i < city.length; i++) {
   
-    let card = document.createElement('section');
     let h2 = document.createElement('h2');
-    let des = document.createElement('p');
     let fig = document.createElement('figure');
+    let figCap = document.createElement('figcaption');
     let pic = document.createElement('picture');
     let image = document.createElement('img');
     
     h2.textContent = city[i].name;
     h2.setAttribute('class', 'headingTwo');
-    des.textContent = city[i].description;    
-    image.setAttribute('src', `images/${city[i].placeholder}`);
+    figCap.textContent = city[i].description;   
+    image.setAttribute('src', `images/${city[i].placeholder}`); 
     image.setAttribute('data-src', `images/${city[i].photo}`);
     image.setAttribute('class', 'townImages');
     image.setAttribute('alt', city[i].name+ "("+city[i].photo + ")");
-    card.setAttribute('class', 'citySec' );
     
-        card.appendChild(h2);
-        card.appendChild(des);
-        card.appendChild(image);
-      
-     document.querySelector('div.cityImg').appendChild(card);
+    
+
+        pic.appendChild(image);
+        document.querySelector('div.cityImg').appendChild(pic);
+        fig.appendChild(h2);
+        fig.appendChild(pic);
+        fig.appendChild(figCap);
+        
+        document.querySelector('div.cityImg').appendChild(fig);
+        
+    
+  
 }
 });
