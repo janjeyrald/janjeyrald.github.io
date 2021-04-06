31a10eae31aa10a612f8cd386253721797a1b907
janjeyrald <u195595992@byui.edu>
2021-04-06 12:26:58 +0800

    city

--- FinalProject/js/city.js
+++ FinalProject/js/city.js
@@ -7,42 +7,28 @@ fetch(requestURL)
 
 .then(function (jsonObject) {
 
-const towns = jsonObject['towns'];
+const city = jsonObject['city'];
     
 
-for (let i=0; i < towns.length; i++) {
+for (let i=0; i < city.length; i++) {
   
     let card = document.createElement('section');
     let h2 = document.createElement('h2');
-    let motto = document.createElement('h3');
-    let year = document.createElement('p');
-    let population = document.createElement('p');
-    let rain = document.createElement('p');
+    let des = document.createElement('p');
     let image = document.createElement('img');
-    let textDiv = document.createElement('div')
     
-    if ((towns[i].name == 'Preston')|| (towns[i].name == 'Fish Haven') || (towns[i].name == 'Soda Springs')) {
-    
-    h2.textContent = towns[i].name;
-    motto.textContent = towns[i].motto;    
-    year.textContent = "Year Founded:" + " " + towns[i].yearFounded;
-    population.textContent = "Population:" + " " + towns[i].currentPopulation;
-    rain.textContent = "Annual Rainfall:" + " " +  towns[i].averageRainfall;
-    image.setAttribute('src', `images/${towns[i].photo}`);
+    h2.textContent = city[i].name;
+    des.textContent = city[i].description;    
+    image.setAttribute('src', `images/${city[i].photo}`);
     image.setAttribute('class', 'townImages');
-    image.setAttribute('alt', towns[i].name+ "("+towns[i].motto + ")");
+    image.setAttribute('alt', city[i].name+ "("+city[i].description + ")");
     h2.setAttribute('class', 'headingTwo');
-    card.setAttribute('class', 'homepageSection' );
-    textDiv.setAttribute('class', 'homepageInfo');
-
-        card.appendChild(textDiv);
-        textDiv.appendChild(h2);
-        textDiv.appendChild(motto);
-        textDiv.appendChild(year);
-        textDiv.appendChild(population);
-        textDiv.appendChild(rain);
-        card.appendChild(image);}
+    card.setAttribute('class', 'cityImgSec' );
+    
+        card.appendChild(h2);
+        card.appendChild(description);
+        card.appendChild(image);
       
-     document.querySelector('div.idahoTowns').appendChild(card);
+     document.querySelector('div.cityImg').appendChild(card);
 }
 });
